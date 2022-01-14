fce21e48d77b70898b93878bab37153994609ca3
FrozenIce0617 <prosdev087@gmail.com>
2022-01-14 14:56:37 +0000

    feat: add eslint and prettier

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,11 +1,11 @@
-import * as Styled from "./Dashboard.style";
+import * as Styled from './Dashboard.style'
 
 const DashboardView: React.FC = () => {
   return (
     <Styled.Container>
       <span>Hello World</span>
     </Styled.Container>
-  );
-};
+  )
+}
 
-export default DashboardView;
+export default DashboardView
