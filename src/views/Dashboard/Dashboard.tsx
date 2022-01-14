accefae8db3641d3858a3123e6b33bf0ff175c2d
FrozenIce0617 <prosdev087@gmail.com>
2022-01-14 14:09:04 +0000

    feat: add styled-components for styling

---
+++ src/views/Dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import * as Styled from "./Dashboard.style";
+
+const DashboardView: React.FC = () => {
+  return (
+    <Styled.Container>
+      <span>Hello World</span>
+    </Styled.Container>
+  );
+};
+
+export default DashboardView;
